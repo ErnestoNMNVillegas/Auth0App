192a71fcec35d68af46be56cc24c54e84a200141
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-06-07 19:05:41 -0500

    auth0 set up

---
+++ src/test/java/com/example/auth0app/Auth0AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth0app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
